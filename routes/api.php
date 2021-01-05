13912d14a9220a01427443931608648ca68c9032
bodroghykristof <bodroghykristof@gmail.com>
2021-01-05 21:18:44 +0100

    init: add empty controller and routes for User model

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::apiResource("mails", "App\Http\Controllers\MailController");
+Route::apiResource("users", "App\Http\Controllers\UserController");
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
