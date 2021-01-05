f91d85593d038e5d58ed6f609f2196ff03418150
bodroghykristof <bodroghykristof@gmail.com>
2021-01-05 14:34:14 +0100

    init: add model, migration and controller for Mail

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mail;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class MailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Mail $mail
+     * @return Response
+     */
+    public function show(Mail $mail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Mail $mail
+     * @return Response
+     */
+    public function update(Request $request, Mail $mail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Mail $mail
+     * @return Response
+     */
+    public function destroy(Mail $mail)
+    {
+        //
+    }
+}
