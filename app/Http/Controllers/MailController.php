72890986b6e651a695465b67d3f180ff1a8114dc
bodroghykristof <bodroghykristof@gmail.com>
2021-01-06 14:31:20 +0100

    feat: join queries to serve uer data as well

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Mail;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 class MailController extends Controller
 {
@@ -22,12 +23,15 @@ class MailController extends Controller
      * Display a mails in user's inbox.
      *
      * @param int $userId
-     * @return void
+     * @return \Illuminate\Support\Collection
      */
     public function inbox(int $userId)
     {
-        return Mail::where("id_user_to", $userId)
+        return DB::table('mails')
+            ->join('users', 'users.id', '=', 'mails.id_user_from')
+            ->where("id_user_to", $userId)
             ->orderByDesc('sent')
+            ->select('mails.*', 'users.name as from', 'users.email as from_email')
             ->get();
     }
 
