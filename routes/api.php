b7bb8b97091bba7f1b846b6f694b50ca7eeaf7d1
bodroghykristof <bodroghykristof@gmail.com>
2021-01-06 13:49:45 +0100

    fix: name of route from 'from' to 'to'

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::apiResource("mails", "App\Http\Controllers\MailController");
-Route::get("mails/from/{userId}", "App\Http\Controllers\MailController@inbox");
+Route::get("mails/to/{userId}", "App\Http\Controllers\MailController@inbox");
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
